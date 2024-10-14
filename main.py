d294bfcc0c42c6cb3f86b04a6031af68de634dc2
Garrett Baltz <garrett.baltz1@copart.com>
2024-10-14 15:04:03 -0500

    Flushed out variable board size capabilities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Simple Tic Tac Toe game with plans to make it goofy
 # By Garrett Baltz
 import numpy as np
+import random
 
 np.random.seed(10072001)
 
@@ -13,49 +14,68 @@ def show_board(board):
     for row in range(len(board)):
         print(board[row])
 
+# Generator to get all sublists from a list
+def get_sublist(line, size):
+    i = 0
+    length = len(line)
+    while i <= length - size:
+        yield line[i:i+size]
+        i += 1
+
+# Gets every possible sublist of a certain size of a board 
+def get_all_sublists(board, win_size):
+    wsublists = []
+
+    # all sublists of rows
+    for row in board:
+        wsublists.append(get_sublist(row, win_size))
+
+    # all sublists of columns
+    tboard = np.transpose(board)
+    for col in tboard.tolist():
+        wsublists.append(get_sublist(col, win_size))
+
+    wsublists.append(get_diags(np.array(board), win_size))
+
+    sublists = []
+    for l in wsublists:
+        for l2 in list(l):
+            sublists.append(list(l2))
+    
+    return sublists
+
+
 # Checks board state to see if there is a win
 def check_win(board, markers, win_size):
     win = False
     winner = ''
+    sublists = get_all_sublists(board, win_size)
     
-    # Rows nontransposed
-    for row in board:
-        row = list(row)
-        if row.count(markers[0]) == len(row):
+    for line in sublists:
+        if line.count(markers[0]) == win_size:
             win = True
             winner = markers[0]
-            return win, winner
-        elif row.count(markers[1]) == len(row):
+            break
+        if line.count(markers[1]) == win_size:
             win = True
             winner = markers[1]
-            return win, winner
-        
-    # Rows transposed
-    for row in np.transpose(board):
-        row = list(row)
-        if row.count(markers[0]) == len(row):
-            win = True
-            winner = markers[0]
-            return win, winner
-        elif row.count(markers[1]) == len(row):
-            win = True
-            winner = markers[1]
-            return win, winner
-        
-    # Diagonals
-    diag = list(np.diag(board))
-    if diag.count(markers[0]) == len(diag):
-        win = True
-        winner = diag[0]
-        return win, winner
-    
-    tdiag = list(np.diag(np.transpose(board)))
-    if tdiag.count(markers[0]) == len(tdiag):
-        win = True
-        winner = tdiag[0]
-        return win, winner
-    
+            break
+
     return win, winner
+
+def get_diags(board, size):
+    if size > board.shape[0]:
+        raise ValueError(f'Cant get size {size} rows, board is {board.shape}')
+    
+    # all sublists of diagonals
+    for d in range(-size, size + 1):
+        yield from get_sublist(np.diag(board, d).tolist(), size)
+
+    # all sublists of flipped diagonals
+    for d in range(-size, size + 1):
+        yield from get_sublist(np.diag(np.fliplr(board), d).tolist(), size)
+
+
 
 # Check if a win is possible
 def check_cat(board, markers):
@@ -66,25 +86,93 @@ def check_cat(board, markers):
     
     return True
 
-def play_again():
-    res = input("Would you like to play again? (y/n)")
-    if res.lower == 'y' or res.lower == 'yes' or res.lower == 'yeah' or res.lower == 'ye':
-        play_tic(board=board, win_size=win_size)
+def play_again(board, win_size):
+    res = input("Would you like to play again? (y/n) ")
+    print(res.lower())
+    if res.lower() == 'y' or res.lower() == 'yes' or res.lower() == 'yeah' or res.lower() == 'ye':
+        print("Play Again")
+        play_tic()
 
 # Play state, rotates through players until win or cat's game
-def play_tic(board, win_size):
+def play_tic():
 
     print("PLAY TIC-TAC-TOE!")
+    # run game
+    while True:
+        try:
+            size = int(input("How big of a board? (x) ")) # Change me for different board sizes
+            win_size = int(input("How many in a row to win? (x) ")) # Change me for length of win (in a row)
+            board=create_board(size)
+            show_board(board)
+            break
+        except:
+            print("Please enter a number, not a string")
+
     p1 = input("Player 1: Enter piece marker -> ")
     p2 = input("Player 2: Enter piece marker -> ")
     markers = [p1, p2]
 
     print("Randomly choosing first player...")
 
-    pnum = np.random.randint(-1, 1)
+    pnum = random.choice([-1, 1])
+    print(pnum)
 
     win, _ = check_win(board, markers, win_size)
     
+    # print(win)
+
+    while not win:
+        player = 1 if pnum == -1 else 2
+        loc_in = input(f"Player {player}'s turn, type target location in form 'row, column' -> ")
+        try:
+            loc = [int(x.strip()) for x in loc_in.split(',')]
+        except:
+            print("Invalid input, try again.")
+            continue
+        print(loc)
+        if board[loc[0]][loc[1]] == '_':
+            board[loc[0]][loc[1]] = p1 if player == 1 else p2
+        else:
+            print("Invalid move, try again.")
+            continue
+        show_board(board)
+
+        win, _ = check_win(board, markers, win_size)
+        if check_cat(board, markers):
+            print("Cat's game!")
+            play_again(board, win_size)
+            exit()
+        pnum *= -1
+    
+    _, x = check_win(board, markers, win_size)
+    if x == p1:
+        winner = 1
+    else:
+        winner = 2
+    
+    print(f"Player {winner} Wins!")
+    play_again(board, win_size)
+
+# Play state, rotates through players until win or cat's game
+def test_tic(board, win_size=3, filled=False):
+
+    print("PLAY TIC-TAC-TOE!")
+    show_board(board)
+    p1 = 'x'
+    p2 = 'o'
+    markers = [p1, p2]
+
+    # print("Randomly choosing first player...")
+
+    pnum = np.random.randint(-1, 1)
+
+    win, winner = check_win(board, markers, win_size)
+
+    print(win, winner)
+    
+    if filled:
+        exit()
+
     # print(win)
 
     while not win:
@@ -119,9 +207,15 @@ def play_tic(board, win_size):
     print(f"Player {winner} Wins!")
     play_again()
 
-# run game
-size = 3 # Change me for different board sizes
-win_size = 3 # Change me for length of win (in a row)
-board = create_board(size)
-show_board(board)
-play_tic(board, win_size)
+play_tic()
+
+brd3 = [['x', 'o', 'x'],
+       ['x', 'x', 'o'],
+       ['x', 'o', 'x']]
+
+brd4 = [['x', 'o', 'x', 'o'],
+       ['x', 'x', 'o', 'x'],
+       ['x', 'o', 'x', 'x'],
+       ['x', 'o', 'x', 'x']]
+
+# test_tic(brd4, 3, filled=True)
